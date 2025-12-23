aa5f058448a2b510c4214c6c4416c8fffabac48a
tlhylmm <tlhylmm@gmail.com>
2025-12-23 20:31:56 +0300

    fix:footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className={styles.socials}>
                 <a href="https://www.instagram.com/gultekinmodaevi/" className={styles.link}>Instagram</a>
                 <a href="https://www.facebook.com/GultekinModaEvi/" className={styles.link}>Facebook</a>
-                <a href="/contact" className={styles.link}>Contact</a>
+                <a href="/contact" className={styles.link}>İletişim</a>
             </div>
             <p className={styles.copyright}>
                 © {new Date().getFullYear()} Gültekin Ataseven. Tüm Hakları Saklıdır.
